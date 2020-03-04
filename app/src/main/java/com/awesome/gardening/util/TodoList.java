2ac5a08907fd243470fc21f1c312f7173aa10163
zhanglulu <zhanglulu@cmhi.chinamobile.com>
2020-03-04 23:45:53 +0800

    TODO on 20200304

--- app/src/main/java/com/awesome/gardening/util/TodoList.java
+++ app/src/main/java/com/awesome/gardening/util/TodoList.java
@@ -1,9 +1,23 @@
 package com.awesome.gardening.util;
 
 public class TodoList {
+
     // TODO: 2020/3/4 what's androidx.navigation.fragment.NavHostFragment
     // TODO: 2020/3/4 what does app:defaultNavHost="true" means?
     // TODO: 2020/3/4 <navigation> ??
+
+
+
+
+    // TODO: 2020/3/4 android:fitsSystemWindows="true">
+    // TODO: 2020/3/4 ?attr/ ??
+
+
+
+
+    // TODO: 2020/3/4 CoordinatorLayout
+    // TODO: 2020/3/4 androidx.viewpager2.widget.ViewPager2
+    // TODO: 2020/3/4 com.google.android.material.appbar.CollapsingToolbarLayout ??
 
 
 }
