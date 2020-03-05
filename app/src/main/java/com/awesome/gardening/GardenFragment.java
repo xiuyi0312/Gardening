38c06bb42bebad6ce049e9d26417e10f9bf8ef64
zhanglulu <zhanglulu@cmhi.chinamobile.com>
2020-03-05 23:43:49 +0800

    finish PlantListFragment's UI and DetailFragment's UI and test the fitsSystemWindows property

--- app/src/main/java/com/awesome/gardening/GardenFragment.java
+++ app/src/main/java/com/awesome/gardening/GardenFragment.java
@@ -11,22 +11,16 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.awesome.gardening.databinding.FragmentGardenBinding;
-import com.awesome.gardening.databinding.FragmentGardenBindingImpl;
-import com.awesome.gardening.databinding.FragmentPlantListBinding;
 
 public class GardenFragment extends Fragment {
+    private String plantDetailExample = "An apple is a sweet, edible fruit produced by an apple tree (Malus pumila). Apple trees are cultivated worldwide, and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today. Apples have been grown for thousands of years in Asia and Europe, and were brought to North America by European colonists. Apples have religious and mythological significance in many cultures, including Norse, Greek and European Christian traditions.<br><br>Apple trees are large if grown from seed. Generally apple cultivars are propagated by grafting onto rootstocks, which control the size of the resulting tree. There are more than 7,500 known cultivars of apples, resulting in a range of desired characteristics. Different cultivars are bred for various tastes and uses, including cooking, eating raw and cider production. Trees and fruit are prone to a number of fungal, bacterial and pest problems, which can be controlled by a number of organic and non-organic means. In 2010, the fruit's genome was sequenced as part of research on disease control and selective breeding in apple production.<br><br>Worldwide production of apples in 2014 was 84.6 million tonnes, with China accounting for 48% of the total.<br><br>(From <a href=\"https://en.wikipedia.org/wiki/Apple\">Wikipedia</a>)";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         FragmentGardenBinding binding = FragmentGardenBinding.inflate(inflater, container, false);
-        TextView plant = binding.plant;
-        plant.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
+        TextView plant = binding.plantDescription;
+        plant.setText(plantDetailExample);
         return binding.getRoot();
     }
 }
--- app/src/main/java/com/awesome/gardening/MainActivity.java
+++ app/src/main/java/com/awesome/gardening/MainActivity.java
@@ -1,10 +1,12 @@
 package com.awesome.gardening;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.navigation.Navigation;
 
 /**
  * the main activity to show my garden and those plants available
@@ -13,6 +15,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION |
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+//        }
         DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+
     }
+
+    /**
+     * 用于拦截Back键
+     * app:defaultNavHost="true"这个属性意味着你的NavGraphFragment将会
+     * 拦截系统Back键的点击事件（因为系统的back键会直接关闭Activity而非切换Fragment）
+     * @return
+     */
+//    @Override
+//    public boolean onSupportNavigateUp() {
+//        return Navigation.findNavController(this, R.id.nav_host).navigateUp();
+//    }
 }
